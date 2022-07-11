6e82193e43e050bad0e094f7e695e5d2ce0ab676
juanMoleroD <juanldimeco@gmail.com>
2022-07-11 14:48:20 +0100

    passed team test 6

--- day-1/single_class_lab_start_code/team_class/src/team.py
+++ day-1/single_class_lab_start_code/team_class/src/team.py
@@ -6,3 +6,9 @@ class Team:
 
     def add_player(self, new_player):
         self.players.append(new_player)
+
+    def has_player(self, search_player):
+        for player in self.players:
+            if player == search_player:
+                return True
+        return False
